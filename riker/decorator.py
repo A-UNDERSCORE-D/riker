c04497bdb7ee7d41cfafcd52a585f0bda142d7e7
A_D <aunderscored@gmail.com>
2022-01-29 16:08:48 +0200

    resolved methods misbehaving when decorated

--- riker/decorator.py
+++ riker/decorator.py
@@ -1,15 +1,12 @@
 from __future__ import annotations
 
 import itertools
-from typing import TYPE_CHECKING, Awaitable, Callable, Sequence, TypeVar
+from typing import Awaitable, Callable, Sequence, TypeVar
 
 from .command import Command
 
-if TYPE_CHECKING:
-    RETURNS = str | list[str] | None
-    TAG_VALUE = tuple[
-        str | list[str], str | list[str], str | list[str] | None, int | None
-    ]
+RETURNS = str | list[str] | None
+TAG_VALUE = tuple[str | list[str], str | list[str], str | list[str] | None, int | None]
 
 _F = TypeVar("_F", bound=Callable[..., Awaitable[RETURNS]] | Callable[..., RETURNS])
 
@@ -45,20 +42,23 @@ def command(
 def scan_for_commands(x: object) -> list[Command]:
     """Find marked functions."""
     out = []
-    for v in itertools.chain(x.__dict__.values(), x.__class__.__dict__.values()):
-        if not hasattr(v, COMMAND_TAG):
+    for attr_name in itertools.chain(dir(x), dir(x.__class__)):
+        attr = getattr(x, attr_name)
+        if not hasattr(attr, COMMAND_TAG):
             continue
 
-        if not callable(v):
-            raise ValueError(f"Decorator used on non-callable object {v} ({v!r})")
+        if not callable(attr):
+            raise ValueError(
+                f"Decorator used on non-callable object {attr} ({attr!r} -- {x!r})"
+            )
 
-        res: TAG_VALUE = getattr(x, COMMAND_TAG)
+        res: TAG_VALUE = getattr(attr, COMMAND_TAG)
 
         (name, help, permissions, arg_count) = res
 
         if isinstance(permissions, str):
             permissions = [permissions]
 
-        out.append(Command(name, help, permissions, arg_count, v))
+        out.append(Command(name, help, permissions, arg_count, attr))
 
     return out
